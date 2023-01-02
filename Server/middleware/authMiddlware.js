eabaaf8846cf03f6e6252afb50234f1b8a52184f
FidaFairus <70806162+FidaFairus@users.noreply.github.com>
2023-01-02 10:52:41 +0530

    first commit

---
+++ Server/middleware/authMiddlware.js
@@ -0,0 +1,51 @@
+const ResponseModel = require('../utilities/responseModel');
+const tokenHandler = require('../utilities/tokenHandlers');
+
+module.exports = (req, res, next) => {
+    if (!req.url.startsWith('/api/user')) {
+        return next();
+    }
+    console.log(req.url);
+    let token = req.headers['authorization'];
+    token = token ? token.split(' ')[1] : null;
+    if (!token) {
+        return res.status(401)
+            .json(new ResponseModel(null, null, ['Unauthorized.']));
+    }
+
+    try {
+        const tokenResult = tokenHandler.verifyToken(token);
+        req.user = tokenResult;
+        return next();
+    }
+    catch (err) {
+        return res.status(401)
+            .json(new ResponseModel(null, null, ['Unauthorized.']));
+    }
+}
+// const ResponseModel = require('../utilities/responseModel');
+// const tokenHandler = require('../utilities/tokenHandlers');
+
+// module.exports = (req, res, next) => {
+//     if(!req.url.startsWith('/api/v1/user')){
+//         return next();
+//     }
+
+//     let token = req.headers['authorization'];
+//     token = token ? token.split(' ')[1] : null;
+
+//     if(!token){
+//         return res.status(401)
+//             .json(new ResponseModel(null, null, ['Unauthorized.']));
+//     }
+
+//     try{
+//         const tokenResult = tokenHandler.verifyToken(token);
+//         req.user = tokenResult;
+//         return next();
+//     }
+//     catch(err){
+//         return res.status(401)
+//             .json(new ResponseModel(null, null, ['Unauthorized.']));
+//     }
+// }
